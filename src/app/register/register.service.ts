bc278d830bfb094d966356cda20761b137f5a67e
devmina96 <68608303+devmina96@users.noreply.github.com>
2023-03-28 20:39:57 +0530

    added new functions except jwt

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -1,13 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { catchError, first, of } from 'rxjs';
 import { User } from './user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegisterService {
-
-  constructor() { }
-  public registerUser(user: User){
-        console.log(user)
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+  public registerUser(user: User) {
+    this.http
+      .post('http://localhost:8181/register/', user, {
+        responseType: 'text' as 'json',
+      })
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          this.router.navigate(['../login'], { relativeTo: this.route });
+        },
+      });
   }
 }
